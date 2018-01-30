e4a8cbdfa73174cfd04e32363d05f56e3cb929be
Dmitry Litvin <leetvin@gmail.com>
2018-01-30 23:47:05 +0500

    Created basic page UI. Next up is the js.

--- js/game/Game2048.js
+++ js/game/Game2048.js
@@ -1,6 +1,8 @@
 export default class Game2048 {
     constructor({element}) {
         element.innerHTML = "ok the game is here";
+        // TODO: use the width of the container and make the height the same
+        // TODO: and put listeners on page resize to update the game size
     }
 
     run() {
